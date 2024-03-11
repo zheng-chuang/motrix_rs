a480bca1a68307cc3d74244c69f0710ccf04f1aa
zhengchuang <zhengchuang_kindle@163.com>
2024-03-11 17:20:09 +0800

    安装less和classnames

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
-import reactLogo from "../assets/react.svg";
-import "./Home.css";
+import classname from 'classnames/bind';
+import cls from 'classnames'
+
+import reactLogo from "../../assets/react.svg";
+
+import style from "./index.module.less";
+
+const cx = classname.bind(style)
 
 function Home() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -13,18 +19,18 @@ function Home() {
   }
 
   return (
-    <div className="container">
+    <div className={cx("container")}>
       <h1>Welcome to Tauri!</h1>
 
-      <div className="row">
+      <div className={cx("row")}>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo vite" alt="Vite logo" />
         </a>
         <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+          <img src="/tauri.svg" className={"logo tauri"} alt="Tauri logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo}  className={cls("logo", cx('react'))} alt="React logo" />
         </a>
       </div>
 
