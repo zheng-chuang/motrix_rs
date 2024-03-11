27e82c979083219b742bf26099ec0efc24a4479a
zhengchuang <zhengchuang_kindle@163.com>
2024-03-11 20:11:49 +0800

    自定义titlebar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 
 function Layout() {
     return <div>
+        <div data-tauri-drag-region style={{ height: '30px', }}></div>
         <h1>Layout</h1>
         <Outlet />
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./styles.css";
+
+import "normalize.css/normalize.css"
+import "./styles.less";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
